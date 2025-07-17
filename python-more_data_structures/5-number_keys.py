68ff9a81b1b0aa308dd1e7d35520d6c11c0ba30a
Joseph-Oke <j.oke@alustudent.com>
2025-07-17 05:48:11 -0700

    Ok

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary to count keys from.
+
+    Returns:
+        int: Number of keys in the dictionary.
+    """
+    return len(a_dictionary)
