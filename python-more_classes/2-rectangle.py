3f8e86cad20894866b2ab84b5646a9e0ee0a2c8f
Joseph-Oke <j.oke@alustudent.com>
2025-07-17 07:27:46 -0700

    ok

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle with width, height, area, and perimeter."""
+"""Module that defines a Rectangle class with width, height, area and perimeter."""
 
 
 class Rectangle:
@@ -50,3 +50,24 @@ class Rectangle:
             value (int): new height value
 
         Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle.
+
+        If width or height is 0, perimeter is 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
