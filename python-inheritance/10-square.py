2111eac239871c858b60a991ad3efb5f0e98c778
Joseph-Oke <j.oke@alustudent.com>
2025-07-17 13:25:27 -0700

    ok

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module defines a Square class that inherits from Rectangle."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Represents a square, inherits from Rectangle."""
+
+    def __init__(self, size):
+        """
+        Initialize a Square.
+
+        Args:
+            size (int): The size of the square's sides.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is <= 0.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
